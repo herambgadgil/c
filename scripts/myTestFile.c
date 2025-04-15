9d9aa9af259397da4b1616358911f05d22d7e31e
G543917 <heramb.gadgil@genmills.com>
2025-04-15 15:29:07 +0530

    first lesson on data-types, declaration, comments and printing

---
+++ scripts/myTestFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// declare variables
+int myFirstVariable = 1;
+char myFirstCharacter = 'H';
+float myFirstFloat = 14.99;
+
+// build a function to output
+int main(){
+    printf("\n\n%c is the Element with atomic number %d and atomic weight %f", myFirstCharacter, myFirstVariable, myFirstFloat);
+    return 0;
+}
